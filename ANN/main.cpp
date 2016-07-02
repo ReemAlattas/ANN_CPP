3e082c34d580e4e3d1365c00f8969052e3bef7ed
ReemAlattas <ralattas@live.com>
2016-07-02 05:43:47 +0300

    first commit

---
+++ ANN/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ANN
+//
+//  Created by Reem Alattas on 7/2/16.
+//  Copyright © 2016 ReemAlattas. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
